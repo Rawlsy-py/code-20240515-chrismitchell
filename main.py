6d8fb9d4830e1200b8cd058668e69c445508e6fc
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2024-05-15 13:34:59 +0100

    feat: Refactor main.py to use data cleaning function

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import typer
 import pandas as pd
+import vamstar.data_cleaning as dc
 
 app = typer.Typer()
 
 
+df = pd.read_csv("test.tsv", sep="\t")
+
+
 @app.command()
-def run_cleaning_process():
+def run_cleaning_process(df):
+    """
+    Run the data cleaning process
+    """
+    dc.mean_population_data(df=df)
     typer.echo("TSV file cleaned successfully!")
 
 
